a215d9c4c14a4a28dde5a954007a744757dc48ee
Maitreyee Nath <55870039+mnath30@users.noreply.github.com>
2021-10-02 08:09:22 +0000

    fix: deleted file

--- index.js
+++ index.js
@@ -28,30 +28,30 @@ function check(question,answer){
 
 //creating objects of questions
 var questionOne={
-  question:"Which is her favourite colour? \n",
+  question:"Which is her favourite colour?\n (yellow, pink, green, black) \n",
   answer:"black"
 }
 
 var questionTwo={
-  question:"Where does she work? \n",
+  question:"Where does she work?\n (TCS, Infosys, LTI, Wipro) \n",
   answer:'tcs'
 }
 
 var questionThree={
-  question:'Which is her favourite holiday destination? (Mountains or beaches or forests) \n',
+  question:'Which is her favourite holiday destination? \n (Mountains, beaches, forests) \n',
   answer:'beaches'
 }
 
 var questionFour={
-  question:'Her favourite TV Series?\n',
+  question:'Her favourite TV Series? \n ( Friends, Young Sheldon, The Middle ) \n',
   answer:'friends' 
 }
 var questionFive={
-  question:'Where do I live?\n',
+  question:'Where does she live? \n (Bangalore, Kolkata, Mumbai, Chennai) \n',
   answer:'mumbai'
 }
 var questionSix={
-  question:'Which is her favourite dish?\n',
+  question:'Which is her favourite dish?\n (pasta, biriyani, pizza, burger)\n',
   answer:'biriyani'
 }
 
@@ -71,3 +71,6 @@ console.log(`Our high scorer is ${highScore.name} :  ${highScore.score}`);
 if (score>=highScore.score){
   console.log("Yay you have beat the high score. Send me a screenshot so I can update your name.");
 }
+
+
+
